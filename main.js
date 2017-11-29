aca0c89a4dbb5fb08cee822d0977853bbf09cd38
Kaiwei Wu <kaiwei-wu@hotmail.com>
2017-11-30 08:38:40 +1300

    updated passwords

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: '580f7828-b95f-415e-a863-1f7f459e1fad',
+    appPassword: 'mfGIVP288%ddimzNPN67*@%'
 });
 
 // Listen for messages from users 
